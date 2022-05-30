14b5012b7b2312b94dff21ee29e8365a25fb31f0
Kevinmuriuki <kevinmuriukiwaweru@gmail.com>
2022-05-30 07:18:33 +0300

    add setup

--- app.py
+++ app.py
@@ -53,7 +53,9 @@ class Artist(db.Model):
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+db.create_all()
+
+  # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
